da0dddfc1a5fa452485c1d6f9c43f73c75941cd5
Matheus <macariniking@gmail.com>
2021-12-31 16:42:36 -0300

    Iniciando implementação de repositorio genérico

---
+++ RestWithDotNet5/RestWithDotNet5/Repository/Generic/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestWithDotNet5.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithDotNet5.Repository.Implementations
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T Update(T item);
+        List<T> FindAll();
+        T FindById(long id);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
