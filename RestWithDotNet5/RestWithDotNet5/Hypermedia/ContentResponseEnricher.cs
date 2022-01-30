c0b536ce66170000e9bdd668aaa26f971d25c135
Matheus <macariniking@gmail.com>
2022-01-30 09:38:56 -0300

    Configurando o HATEOAS Enricher para Suportar o PagedSearchVO

--- RestWithDotNet5/RestWithDotNet5/Hypermedia/ContentResponseEnricher.cs
+++ RestWithDotNet5/RestWithDotNet5/Hypermedia/ContentResponseEnricher.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Routing;
 using RestWithDotNet5.Hypermedia.Abstract;
+using RestWithDotNet5.Hypermedia.Utils;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestWithDotNet5.Hypermedia
@@ -17,7 +19,7 @@ namespace RestWithDotNet5.Hypermedia
 
         public bool CanEnrich(Type contentType)
         {
-            return contentType == typeof(T) || contentType == typeof(List<T>);
+            return contentType == typeof(T) || contentType == typeof(List<T>) || contentType == typeof(PagedSearchVO<T>);
         }
 
         protected abstract Task EnrichModel(T content, IUrlHelper urlHelper);
@@ -42,6 +44,13 @@ namespace RestWithDotNet5.Hypermedia
                 {
                     ConcurrentBag<T> bag = new ConcurrentBag<T>(collection);
                     Parallel.ForEach(bag, (element) =>
+                    {
+                        EnrichModel(element, urlHelper);
+                    });
+                }
+                else if (okObjectResult.Value is PagedSearchVO<T> pagedSearch)
+                {
+                    Parallel.ForEach(pagedSearch.List.ToList(), (element) =>
                     {
                         EnrichModel(element, urlHelper);
                     });
