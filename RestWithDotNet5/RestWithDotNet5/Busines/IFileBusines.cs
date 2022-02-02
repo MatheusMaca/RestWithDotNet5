27e881124e301e1ee25430412f6fde243567a4ea
Matheus <macariniking@gmail.com>
2022-02-01 21:26:08 -0300

    Implementando o Método Responsável por Salvar os Arquivos em disco

---
+++ RestWithDotNet5/RestWithDotNet5/Busines/IFileBusines.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithDotNet5.Data.VO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestWithDotNet5.Busines
+{
+    public interface IFileBusines
+    {
+        public byte[] GetFile(string fileName);
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public List<FileDetailVO> SaveFilesToDisk(IList<IFormFile> files);
+    }
+}
---
+++ RestWithDotNet5/RestWithDotNet5/Busines/Implementations/FileBusines.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithDotNet5.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RestWithDotNet5.Busines.Implementations
+{
+    public class FileBusines : IFileBusines
+    {
+        private readonly string _basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusines(IHttpContextAccessor context)
+        {
+            _context = context;
+            _basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            FileDetailVO fileDetail = new FileDetailVO();
+            var fileType = Path.GetExtension(file.FileName);
+            var baseUrl = _context.HttpContext.Request.Host;
+
+            if (fileType.ToLower() == ".pdf" || fileType.ToLower() == ".jpg" || fileType.ToLower() == ".png" || fileType.ToLower() == ".jpeg")
+            {
+                var docName = Path.GetFileName(file.FileName);
+
+                if (file != null && file.Length > 0)
+                {
+                    var destination = Path.Combine(_basePath, "", docName);
+                    fileDetail.DocumentName = docName;
+                    fileDetail.DocType = fileType;
+                    fileDetail.DocUrl = Path.Combine(baseUrl + "/api/file/v1" + fileDetail.DocumentName);
+
+                    using var stream = new FileStream(destination, FileMode.Create);
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return fileDetail;
+        }
+
+        public byte[] GetFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FileDetailVO> SaveFilesToDisk(IList<IFormFile> files)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
